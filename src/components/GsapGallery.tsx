63bdbe30b937fd7d4720ad3f0c4c4ad4394b694f
Dima Portenko <troublesohard@gmail.com>
2025-08-09 20:14:48 +0300

    WIP update ts types

--- src/components/GsapGallery.tsx
+++ src/components/GsapGallery.tsx
@@ -31,7 +31,7 @@ export const GsapGallery = () => {
 
       let activeElement: HTMLElement | undefined;
       const loop = horizontalLoop(boxes, {
-        paused: false,
+        paused: true,
         draggable: true, // make it draggable
         center: true, // active element is the one in the center of the container rather than th left edge
         repeat: -1,
@@ -88,7 +88,7 @@ Features:
           center: boolean; // active element is the one in the center of the container rather than th left edge
           repeat?: number | undefined;
           speed?: number;
-          snap?: number | boolean;
+          snap?: number | undefined;
           paddingRight?: string;
           reversed?: boolean;
         }
@@ -98,158 +98,170 @@ Features:
         config = config || {};
         gsap.context(() => {
           // use a context so that if this is called from within another context or a gsap.matchMedia(), we can perform proper cleanup like the "resize" event handler on the window
-          let onChange = config.onChange,
-            lastIndex = 0,
-            tl = gsap.timeline({
-              repeat: config.repeat,
-              onUpdate:
-                onChange &&
-                function () {
-                  let i = tl.closestIndex();
-                  if (lastIndex !== i) {
-                    lastIndex = i;
-                    onChange(items[i], i);
-                  }
-                },
-              paused: config.paused,
-              defaults: { ease: "none" },
-              onReverseComplete: () => {
-                tl.totalTime(tl.rawTime() + tl.duration() * 100);
+          let onChange = config.onChange;
+          let lastIndex = 0;
+          let tl = gsap.timeline({
+            repeat: config.repeat,
+            onUpdate:
+              onChange &&
+              function () {
+                let i = tl.closestIndex();
+                if (lastIndex !== i) {
+                  lastIndex = i;
+                  onChange(items[i], i);
+                }
               },
-            }),
-            length = items.length,
-            startX = items[0]?.offsetLeft,
-            times: number[] = [],
-            widths: number[] = [],
-            spaceBefore: number[] = [],
-            xPercents: number[] = [],
-            curIndex = 0,
-            indexIsDirty = false,
-            center = config.center,
-            pixelsPerSecond = (config.speed || 1) * 100,
-            snap =
-              config.snap === false
-                ? (
-                    snapConfig: number | number[] | gsap.utils.SnapNumberConfig
-                  ) => snapConfig
-                : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
-            timeOffset = 0,
-            container =
-              center === true
-                ? items[0].parentNode
-                : gsap.utils.toArray(center)[0] || items[0].parentNode,
-            totalWidth,
-            getTotalWidth = () =>
-              items[length - 1].offsetLeft +
-              (xPercents[length - 1] / 100) * widths[length - 1] -
-              startX +
-              spaceBefore[0] +
-              items[length - 1].offsetWidth *
-                gsap.getProperty(items[length - 1], "scaleX") +
-              (parseFloat(config.paddingRight) || 0),
-            populateWidths = () => {
-              let b1 = container.getBoundingClientRect(),
-                b2;
-              items.forEach((el, i) => {
-                widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
-                xPercents[i] = snap(
-                  (parseFloat(gsap.getProperty(el, "x", "px")) / widths[i]) *
-                    100 +
-                    gsap.getProperty(el, "xPercent")
+            paused: config.paused,
+            defaults: { ease: "none" },
+            onReverseComplete: () => {
+              tl.totalTime(tl.rawTime() + tl.duration() * 100);
+            },
+          });
+          let length = items.length;
+          let startX = items[0]?.offsetLeft;
+          let times: number[] = [];
+          let widths: number[] = [];
+          let spaceBefore: number[] = [];
+          let xPercents: number[] = [];
+          let curIndex = 0;
+          let indexIsDirty = false;
+          let center = config.center;
+          let pixelsPerSecond = (config.speed || 1) * 100;
+          let snap =
+            config.snap === undefined
+              ? (snapConfig: number | number[] | gsap.utils.SnapNumberConfig) =>
+                  snapConfig
+              : gsap.utils.snap(config.snap || 1); // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+          let timeOffset = 0;
+          let container: HTMLElement | null = items[0]
+            ?.parentNode as HTMLElement;
+          // center === true
+          //   ? items[0]?.parentNode
+          //   : gsap.utils.toArray(center)[0] || items[0]?.parentNode;
+          let totalWidth: number;
+          let getTotalWidth = () => {
+            return (
+              Number(items?.[length - 1]?.offsetLeft) +
+              ((xPercents?.[length - 1] ?? 0) / 100) *
+                (widths?.[length - 1] ?? 0) -
+              (startX ?? 0) +
+              (spaceBefore?.[0] ?? 0) +
+              (items?.[length - 1]?.offsetWidth ?? 0) *
+                Number(gsap.getProperty(items[length - 1] ?? "", "scaleX")) +
+              (parseFloat(config.paddingRight || "0") || 0)
+            );
+          };
+          let populateWidths = () => {
+            let b1 = container?.getBoundingClientRect();
+            let b2;
+            items.forEach((el, i) => {
+              widths[i] = parseFloat(
+                gsap.getProperty(el, "width", "px").toString()
+              );
+              xPercents[i] = Number(snap(
+                (parseFloat(gsap.getProperty(el, "x", "px").toString()) /
+                  widths[i]) *
+                  100 +
+                  Number(gsap.getProperty(el, "xPercent"))
+              ));
+              b2 = el.getBoundingClientRect();
+              spaceBefore[i] = b2.left - (i ? b1.right : b1.left);
+              b1 = b2;
+            });
+            gsap.set(items, {
+              // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+              xPercent: (i) => xPercents[i] ?? 0,
+            });
+            totalWidth = getTotalWidth();
+          };
+          let timeWrap: (time: number) => number;
+          let populateOffsets = () => {
+            timeOffset = center
+              ? (tl.duration() * (container.offsetWidth / 2)) / totalWidth
+              : 0;
+            center &&
+              times.forEach((t, i) => {
+                times[i] = timeWrap(
+                  (tl.labels?.[`label${i}`] ?? 0) +
+                    (tl.duration() * (widths[i] ?? 0)) / 2 / (totalWidth ?? 0) -
+                    timeOffset
                 );
-                b2 = el.getBoundingClientRect();
-                spaceBefore[i] = b2.left - (i ? b1.right : b1.left);
-                b1 = b2;
               });
-              gsap.set(items, {
-                // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-                xPercent: (i) => xPercents[i],
-              });
-              totalWidth = getTotalWidth();
-            },
-            timeWrap,
-            populateOffsets = () => {
-              timeOffset = center
-                ? (tl.duration() * (container.offsetWidth / 2)) / totalWidth
-                : 0;
-              center &&
-                times.forEach((t, i) => {
-                  times[i] = timeWrap(
-                    tl.labels["label" + i] +
-                      (tl.duration() * widths[i]) / 2 / totalWidth -
-                      timeOffset
-                  );
-                });
-            },
-            getClosest = (values, value, wrap) => {
-              let i = values.length,
-                closest = 1e10,
-                index = 0,
-                d;
-              while (i--) {
-                d = Math.abs(values[i] - value);
-                if (d > wrap / 2) {
-                  d = wrap - d;
-                }
-                if (d < closest) {
-                  closest = d;
-                  index = i;
-                }
+          };
+          let getClosest = (values: number[], value: number, wrap: number) => {
+            let i = values.length,
+              closest = 1e10,
+              index = 0,
+              d;
+            while (i--) {
+              d = Math.abs(values[i] ?? 0 - value);
+              if (d > wrap / 2) {
+                d = wrap - d;
               }
-              return index;
-            },
-            populateTimeline = () => {
-              let i, item, curX, distanceToStart, distanceToLoop;
-              tl.clear();
-              for (i = 0; i < length; i++) {
-                item = items[i];
-                curX = (xPercents[i] / 100) * widths[i];
-                distanceToStart =
-                  item.offsetLeft + curX - startX + spaceBefore[0];
-                distanceToLoop =
-                  distanceToStart +
-                  widths[i] * gsap.getProperty(item, "scaleX");
-                tl.to(
-                  item,
+              if (d < closest) {
+                closest = d;
+                index = i;
+              }
+            }
+            return index;
+          };
+          let populateTimeline = () => {
+            let i, item, curX, distanceToStart, distanceToLoop;
+            tl.clear();
+            for (i = 0; i < length; i++) {
+              item = items[i];
+              curX = ((xPercents[i] ?? 0) / 100) * (widths[i] ?? 0);
+              distanceToStart =
+                (item?.offsetLeft ?? 0) + curX - (startX ?? 0) + (spaceBefore[0] ?? 0);
+              distanceToLoop =
+                distanceToStart + (widths[i] ?? 0) * Number(gsap.getProperty(item ?? "", "scaleX"));
+                // @ts-ignore
+              tl.to(
+                item ?? null,
+                {
+                  xPercent: snap(((curX - distanceToLoop) / (widths[i] ?? 0)) * 100),
+                  duration: distanceToLoop / pixelsPerSecond,
+                },
+                0
+              )
+                // @ts-ignore
+                .fromTo(
+                  item ?? null,
                   {
-                    xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
-                    duration: distanceToLoop / pixelsPerSecond,
+                    xPercent: snap(
+                      (((curX - distanceToLoop + totalWidth) / (widths[i] ?? 0)) * 100)
+                    ),
                   },
-                  0
+                  {
+                    xPercent: xPercents[i],
+                    duration:
+                      (curX - distanceToLoop + totalWidth - curX) /
+                      pixelsPerSecond,
+                    immediateRender: false,
+                  },
+                  distanceToLoop / pixelsPerSecond
                 )
-                  .fromTo(
-                    item,
-                    {
-                      xPercent: snap(
-                        ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
-                      ),
-                    },
-                    {
-                      xPercent: xPercents[i],
-                      duration:
-                        (curX - distanceToLoop + totalWidth - curX) /
-                        pixelsPerSecond,
-                      immediateRender: false,
-                    },
-                    distanceToLoop / pixelsPerSecond
-                  )
-                  .add("label" + i, distanceToStart / pixelsPerSecond);
-                times[i] = distanceToStart / pixelsPerSecond;
-              }
-              timeWrap = gsap.utils.wrap(0, tl.duration());
-            },
-            refresh = (deep) => {
-              let progress = tl.progress();
-              tl.progress(0, true);
-              populateWidths();
-              deep && populateTimeline();
-              populateOffsets();
-              deep && tl.draggable && tl.paused()
-                ? tl.time(times[curIndex], true)
-                : tl.progress(progress, true);
-            },
-            onResize = () => refresh(true),
-            proxy;
+                .add("label" + i, distanceToStart / pixelsPerSecond);
+              times[i] = distanceToStart / pixelsPerSecond;
+            }
+            timeWrap = gsap.utils.wrap(0, tl.duration());
+          };
+
+          let refresh = (deep: boolean) => {
+            let progress = tl.progress();
+            tl.progress(0, true);
+            populateWidths();
+            deep && populateTimeline();
+            populateOffsets();
+            deep && tl.draggable && tl.paused()
+              ? tl.time(times[curIndex] ?? 0, true)
+              : tl.progress(progress, true);
+          };
+          let onResize = () => refresh(true);
+          let proxy: HTMLElement | null = null;
+
+          // --- vars ends here ---
+
           gsap.set(items, { x: 0 });
           populateWidths();
           populateTimeline();
